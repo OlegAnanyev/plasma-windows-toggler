67238ddea93b52419b18a98ea9733508858e23c7
Oleg Ananyev <32748936+OlegAnanyev@users.noreply.github.com>
2026-05-27 16:29:10 +0300

    Update main.js

--- contents/code/main.js
+++ contents/code/main.js
@@ -3,7 +3,7 @@ const config = {
   shortcutIdPrefix: "AppToggler1",
   apps: [
     {"shortcut": "Meta+X", "query": "firefox", "resourceName": "firefox", "caption": "firefox"},
-    {"shortcut": "Meta+B", "query": "chromium", "caption": "Chromium"},
+    {"shortcut": "Meta+B", "query": "zen", "resourceName": "zen", "caption": "Zen Browser"},
     {"shortcut": "Meta+E", "query": "dolphin", "resourceName": "dolphin", "caption": "Dolphin"},
     {"shortcut": "Meta+T", "query": "termius", "resourceName": "termius", "caption": "termius"},
     {"shortcut": "Meta+V", "query": "vscode", "resourceName": "electron code-oss", "caption": "Code - OSS"},
